45de7c015e33ebb464ab2ac88eb216823bc52471
Diogo <diogoangelim@gmail.com>
2020-04-14 22:48:34 +0800

    fix assets thing

--- wp-content/themes/vesple/js/wizard-assets.js
+++ wp-content/themes/vesple/js/wizard-assets.js
@@ -250,6 +250,33 @@ jQuery(document).ready(function($) {
       }
 
       $('.assets-popup-overlay').remove();
+
+      $(document).on('click', '#edit-shares-' + suggestion.symbol.replace(/\./g, '\\\.'), function() {
+        var template = wp.template('selected-assets');
+        var quantity = $(this).parent().find('[name="stocks-amount"]').val();
+        var symbol =
+
+        var content = {
+          symbol: suggestion.symbol,
+          quantity: quantity,
+        }
+
+        console.log(content);
+
+        $('#selected-assets').append(template(content));
+
+        $('#save-quantity-' + data.symbol.replace(/\./g, '\\\.')).on('click', function(event) {
+          event.preventDefault();
+
+          var quantity = $(this).parent().find('[name="stocks-amount"]').val();
+
+          $('#item-' + suggestion.symbol.replace(/\./g, '\\\.')).find('[name="stock-quantity[]"]').val(quantity);
+          $('#item-' + suggestion.symbol.replace(/\./g, '\\\.') + ' span b').text(quantity);
+          $('.assets-popup-overlay').remove();
+
+        });
+
+      });
     });
 
     $('#close-assets-popup').on('click', function(event) {
